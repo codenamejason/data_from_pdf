704169bf61cad33709d3b3d9f9d232e95c35754d
Jason Romero <codenamejason@users.noreply.github.com>
2014-11-08 22:16:31 -0500

    Create pyPDFCtrl.js

---
+++ pyPDFCtrl.js
@@ -0,0 +1,15 @@
+/**
+ * @ngdoc controller
+ * @name pyPDF.controller:pyPDFCtrl
+ *
+ * @description
+ * _Please update the description and dependencies._
+ *
+ * @requires $scope
+ * */
+
+
+angular.module('pyPDF')
+    .controller('pyPDFCtrl', function($scope){
+
+});
